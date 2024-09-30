cc5cd6e12f34b3b37fbe37db4d5001e9e5299d0a
Klas-Norling <klasaman99@hotmail.com>
2024-09-30 17:53:44 +0200

    Fixed Store RPC

---
+++ kademlia/helper.go
@@ -0,0 +1,43 @@
+package kademlia
+
+import (
+	"crypto/sha1"
+	"encoding/hex"
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+)
+
+func generateHashforNode() string {
+
+	address := returnIpAddress()
+
+	//hash our ip address
+	hashed_addrs := sha1.New()
+	hashed_addrs.Write([]byte(string(address)))
+	sha1_addrs := hex.EncodeToString(hashed_addrs.Sum(nil))
+
+	return sha1_addrs
+}
+
+func returnIpAddress() string {
+	//fetch our ip address
+	hostname, err1 := os.Hostname()
+	hostid, err := net.LookupIP(hostname)
+	ip_array := string(hostid[0])
+	last_number := ip_array[len(ip_array)-1]
+
+	port1, err := strconv.Atoi("8080")
+	port2, err := strconv.Atoi(fmt.Sprintf("%v", last_number))
+
+	port := port1 + port2
+
+	address := fmt.Sprintf("%v:", hostid[0]) + fmt.Sprintf("%v", port)
+
+	UNUSED(err1, err)
+	fmt.Println("IPADDRESS: ", address)
+	return address
+}
+
+func UNUSED(x ...interface{}) {}
--- kademlia/kademlia.go
+++ kademlia/kademlia.go
@@ -1,6 +1,31 @@
 package kademlia
 
+import (
+	"crypto/sha1"
+	"encoding/hex"
+)
+
 type Kademlia struct {
+	data map[string][]byte
+	rt   RoutingTable
+	//Network interface?
+}
+
+// Initializes a node, creates a routingtable and assigns struct values
+func initializeNode() Kademlia {
+	id := NewKademliaID(generateHashforNode())
+	contact := NewContact(id, returnIpAddress())
+
+	var rt RoutingTable = *NewRoutingTable(contact)
+
+	kademlia := Kademlia{
+		//NetworkInterface: network,
+		rt:   rt,
+		data: make(map[string][]byte),
+	}
+
+	return kademlia
+
 }
 
 func (kademlia *Kademlia) LookupContact(target *Contact) {
@@ -11,6 +36,13 @@ func (kademlia *Kademlia) LookupData(hash string) {
 	// TODO
 }
 
+// Takes data and hashes it to a 128 bit value then converts into hexadecimal
+// Note: Need to add a handle RPC file seperates lookupdata, store and lookupcontact
 func (kademlia *Kademlia) Store(data []byte) {
-	// TODO
+	hash := sha1.New()
+	hash.Write(data)
+	sha1_data := hex.EncodeToString(hash.Sum(nil))
+
+	kademlia.data[sha1_data] = data
+
 }
